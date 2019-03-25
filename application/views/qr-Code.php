fcba0dbb2572504c25aa7a1a1df5159ba3a4ec5e
Varun Mamtani (mamtaniv) <mamtaniv@coventry.ac.uk>
2019-03-25 03:21:50 +0000

    Update qr-code.php

--- application/views/qr-Code.php
+++ application/views/qr-Code.php
@@ -1,3 +1,25 @@
+<html lang="en">
+  <head>
+    <script type="text/javascript" src="grid.js"></script>
+    <script type="text/javascript" src="version.js"></script>
+    <script type="text/javascript" src="detector.js"></script>
+    <script type="text/javascript" src="formatinf.js"></script>
+    <script type="text/javascript" src="errorlevel.js"></script>
+    <script type="text/javascript" src="bitmat.js"></script>
+    <script type="text/javascript" src="datablock.js"></script>
+    <script type="text/javascript" src="bmparser.js"></script>
+    <script type="text/javascript" src="datamask.js"></script>
+    <script type="text/javascript" src="rsdecoder.js"></script>
+    <script type="text/javascript" src="gf256poly.js"></script>
+    <script type="text/javascript" src="gf256.js"></script>
+    <script type="text/javascript" src="decoder.js"></script>
+    <script type="text/javascript" src="qrcode.js"></script>
+    <script type="text/javascript" src="findpat.js"></script>
+    <script type="text/javascript" src="alignpat.js"></script>
+    <script type="text/javascript" src="databr.js"></script>
+    <script type="text/javascript" src="customjs.js"></script>
+  </head>
+  <body>
 <?php //Varun Page ?>
 <?php
 /**
@@ -26,5 +48,11 @@ for ($x=0; $x < count($json); $x++) {
 }
 ?>
 
-<h2>Container</h2>
+<h2>Qr Code Scanner</h2>
 <a><?php echo $json[0]['name'] ?></a>
+<div class="video-container">
+  <video id="video-preview"></video>
+  <canvas id="qr-canvas" class="hidden" ></canvas>
+</div>
+    </body>
+</html>
