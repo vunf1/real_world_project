a04bb3e784dab3c294a768c7327969fd0006511c
Joao de Oliveira Maia <deoiveij@coventry.ac.uk>
2019-03-25 02:24:53 +0000

    final

--- application/views/navigateCampus.php
+++ application/views/navigateCampus.php
@@ -123,4 +123,33 @@ function callNav(){//Joao
 </script>
 <div id="suggestions">
 
+
+
+
+
+
+
+
+
+<div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img style="width:100%;height:auto;" class="d-block w-80" src="assets/careers-fair-event.png" alt="First slide">
+    </div>
+    <div class="carousel-item">
+      <img style="width:100%;height:auto;" class="d-block w-80" src="assets/graduation.png" alt="Second slide">
+    </div>
+    <div class="carousel-item">
+      <img style="width:100%;height:auto;" class="d-block w-80" src="assets/freshers.png" alt="Third slide">
+    </div>
+  </div>
+  <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="sr-only">Previous</span>
+  </a>
+  <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="sr-only">Next</span>
+  </a>
+</div>
 </div>
